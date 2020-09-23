3fe97f979896fabb47b151e915c662c3fa535d10
xm181 <1678345383@qq.com>
2020-09-23 21:03:36 +0800

    完成创建app，基本设置等

---
+++ swiper/config.py
@@ -0,0 +1,8 @@
+'''程序逻辑配置，及第三方平台配置'''
+
+# 赛迪云通信设置
+SD_APPID = '48182'
+SD_APPKEY = '15d3274309e4a031e766d3f91835c94c'
+SD_PROJECT = '2OuxR3'  # 短信模板的 ID
+SD_SIGN_TYPE = 'md5'
+SD_API = 'https://api.mysubmail.com/message/xsend.json'
